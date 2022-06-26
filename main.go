0353f620b332b0f1f00ee67ca1672b469cec9dee
suzakutakumi <suzakutakumi3@gmail.com>
2022-06-26 23:31:40 +0900

    IP取得テスト

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", index)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+func index(c echo.Context) error {
+	return c.String(http.StatusOK, c.RealIP())
+}
